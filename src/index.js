757058b06646244c94f16a3d817f9b9eefa11ef6
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2022-03-07 09:26:31 -0500

    update package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 #!/usr/bin/env node
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config.js");
 
-console.log("testing");
+const args = process.argv.slice(2);
+
+if (args[0] === "build") {
+    webpack(webpackConfig, (err, stats) => {
+        if (err) {
+            console.error(err.stack || err);
+            if (err.details) {
+                console.error(err.details);
+            }
+            return;
+        }
+
+        const info = stats.toJson();
+
+        if (stats.hasErrors()) {
+            console.error(info.errors);
+        }
+
+        if (stats.hasWarnings()) {
+            console.warn(info.warnings);
+        }
+
+        // Log result...
+    });
+} else {
+    throw new Error(
+        "The only currently supported command is: delivery-scripts build"
+    );
+}
